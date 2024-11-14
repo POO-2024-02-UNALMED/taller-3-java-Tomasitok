3cf99764db0ef68a23bef32a637aabdf0c511520
Tomas Aristizabal Gomez <tomasaristizabalgomez@gmail.com>
2024-11-13 22:01:27 -0500

    Clases creadas

---
+++ src/main/java/taller3/televisores/TV.java
@@ -0,0 +1,110 @@
+package taller3.televisores;
+
+/**
+ *
+ * @author tomas
+ */
+            
+public class TV {
+    private Marca marca;
+    private int canal = 1;
+    private int precio = 500;
+    private boolean estado;
+    private int volumen = 1;
+    private Control control;
+    private static int numTV;
+
+    public TV(Marca marca, boolean estado) {
+        this.marca = marca;
+        this.estado = estado;
+    }
+    
+    public void turnOn(){
+        estado = true;
+    }
+    
+    public void turnOff(){
+        estado = false;
+    }
+    
+    public void canalUp(){
+        if(estado && canal < 120){
+            canal += 1;
+        }
+    }
+    
+    public void canalDown(){
+        if(estado && canal > 1){
+            canal -= 1;
+        }
+    }
+    
+    public void volumenUp(){
+        if(estado && volumen < 7){
+            volumen += 1;
+        }
+    }
+    
+    public void volumenDown(){
+        if(estado && volumen > 0){
+            volumen -= 1;
+        }
+    }
+    
+    public boolean getEstado(){
+        return estado;
+    }
+    
+    public Marca getMarca() {
+        return marca;
+    }
+
+    public int getCanal() {
+        return canal;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public int getVolumen() {
+        return volumen;
+    }
+
+    public Control getControl() {
+        return control;
+    }
+    
+    public static int getNumTV() {
+        return numTV;
+    }
+
+    public void setMarca(Marca marca) {
+        this.marca = marca;
+    }
+
+    public void setCanal(int canal) {
+        if(canal >= 1 && canal <= 120){
+            this.canal = canal;
+        }
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public void setVolumen(int volumen) {
+        if(volumen >= 0 && volumen <= 7){
+            this.volumen = volumen;
+        }
+    }
+
+    public void setControl(Control control) {
+        this.control = control;
+    }
+
+    public static void setNumTV(int numTV) {
+        TV.numTV = numTV;
+    }    
+    
+}
